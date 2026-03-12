03cefb8aaa393030f8521f78c6b9a82cf2a42162
nullByte <275527903+kevin-klemm@users.noreply.github.com>
2026-03-12 00:00:57 -0400

    Initial Commit

---
+++ volume-slider/volume_sliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  volume_sliderApp.swift
+//  volume-slider
+//
+//  Created by Kevin Klemm on 3/12/26.
+//
+
+import SwiftUI
+
+@main
+struct volume_sliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
